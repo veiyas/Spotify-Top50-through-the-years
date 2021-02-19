62047390263e35310c796e5cb7dc655cf1d38fac
pannacotta98 <algot.sandahl@gmail.com>
2021-02-19 12:18:32 +0100

    Fix data filename that i accidentally changed last commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { csv, select } from 'd3';
 import ParallelCoord from './ParallelCoord';
 import RadarPlot from './RadarPlot';
-import Timeline from './Timeline'
+import Timeline from './Timeline';
 import './style.scss';
 
 const main = async () => {
@@ -11,9 +11,12 @@ const main = async () => {
     const data = await csv('data/top50s.csv');
 
     const pc = new ParallelCoord(data, 'parallel-coord', propsToUsePC);
-    const rp = new RadarPlot(data[Math.floor(Math.random() * data.length)], 'radar-plot', propsToUseRP);
+    const rp = new RadarPlot(
+      data[Math.floor(Math.random() * data.length)],
+      'radar-plot',
+      propsToUseRP
+    );
     const tl = new Timeline(data, 'timeline', propsToUseTL);
-
   } catch (err) {
     console.error(err);
   }
